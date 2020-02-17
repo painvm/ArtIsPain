5ca82561fa25692a59e9e6bdb310fad27aa4bfb8
Vadim Melnikov <vadim.melnikov1996@gmail.com>
2020-02-17 11:21:34 +0000

    - Deleted unused Database project

--- Database/Tables/Image.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Image]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
-    [ObjectTypeId] INT NOT NULL, 
-    [Content] IMAGE NOT NULL,
-
-	CONSTRAINT FK_Image_ObjectType FOREIGN KEY([ObjectTypeId]) REFERENCES dbo.[ObjectType]([Id]),
-)
--- Database/Tables/ObjectType.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ObjectType]
-(
-	[Id] INT NOT NULL, 
-    [ParentObjectTypeId] INT NULL, 
-    [Code] NVARCHAR(50) NOT NULL UNIQUE, 
-    [Name] NVARCHAR(64) NOT NULL UNIQUE
-
-	CONSTRAINT PK_ObjectType PRIMARY KEY([Id])
-)
