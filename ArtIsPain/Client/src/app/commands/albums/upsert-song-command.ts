04b7ab6c67313ac4e0c779e26db114866f955d4a
Vadim Melnikov <vadim.melnikov1996@gmail.com>
2020-04-14 18:29:06 +0300

    - Implemented adding/updating songs on edit album page

--- ArtIsPain/Client/src/app/commands/albums/upsert-song-command.ts
+++ ArtIsPain/Client/src/app/commands/albums/upsert-song-command.ts
@@ -1,11 +1,11 @@
-import { Time } from '@angular/common';
-import { IUpsertEntityCommand } from 'src/app/_interfaces/i-upsert-entity-command';
+import { Time } from "@angular/common";
+import { IUpsertEntityCommand } from "src/app/_interfaces/i-upsert-entity-command";
 
-export class UpsertSongCommand implements IUpsertEntityCommand
-{
-    EntityId?: string;
-    AlbumId?: string;
-    Title: string;
-    Order: number;
-    Length: Time;
+export class UpsertSongCommand implements IUpsertEntityCommand {
+  EntityId?: string;
+  AlbumId?: string;
+  Title: string;
+  Order: number;
+  Length: Time;
+  IsEditMode = false;
 }
